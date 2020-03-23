a8da6ad20c4a0628a68be921ccc2601faf9e0eba
dwkim <dwkim@microsoft.com>
2020-03-23 09:36:58 +0900

    update code for push

---
+++ dist/type-setting.js
@@ -0,0 +1,8 @@
+"use strict";
+// type Person = {
+//     name: string;
+//     age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+//   };
+var people = [person, expert];
+var color = 'red';
+var colors = ['red', 'orange'];
